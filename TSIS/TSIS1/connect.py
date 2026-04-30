e8f9dd2d40bd76d21440a4d2102f41479db7241d
aiphonaison-a11y <aiphon.aison@gmail.com>
2026-04-30 12:10:03 +0500

    Update

--- TSIS/TSIS1/connect.py
+++ TSIS/TSIS1/connect.py
@@ -1,5 +1,11 @@
 import psycopg2
 from config import DB_CONFIG
 
-def get_connection():
-    return psycopg2.connect(**DB_CONFIG)
+
+def connect():
+    try:
+        conn = psycopg2.connect(**DB_CONFIG)
+        return conn
+    except Exception as e:
+        print("Connection error:", e)
+        return None
